795f6c00632cd9fd42f3bd0c561582fe59666195
OODesigns <michael.lindre@oodesigns.co.uk>
2023-09-18 20:32:15 +0100

    Initial commit

--- lib/src/test/java/function/S3FileLoaderTest.java
+++ lib/src/test/java/function/S3FileLoaderTest.java
@@ -1,9 +1,6 @@
 package function;
 
-import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class S3FileLoaderTest {
 
