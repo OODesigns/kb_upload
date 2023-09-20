70a31300e4ffc1c79fb2c26a8b0ea49d5ab45cb3
OODesigns <michael.lindre@oodesigns.co.uk>
2023-09-20 15:49:06 +0100

    basic schema validation

--- lib/src/main/java/function/HandleValidation.java
+++ lib/src/main/java/function/HandleValidation.java
@@ -4,21 +4,20 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+import kb_upload.*;
 
 public class HandleValidation implements RequestHandler<S3Event, String> {
     @Override
     public String handleRequest(final S3Event event, final Context context) {
         final LambdaLogger logger = context.getLogger();
-        final S3EventNotification.S3EventNotificationRecord record = event.getRecords().get(0);
-        final String srcBucket = record.getS3().getBucket().getName();
-        // Object key may have spaces or unicode non-ASCII characters.
-        final String srcKey = record.getS3().getObject().getUrlDecodedKey();
 
-        logger.log("RECORD: " + record);
-        logger.log("SOURCE BUCKET: " + srcBucket);
-        logger.log("SOURCE KEY: " + srcKey);
-        // log execution details
-        return srcBucket + "/" + srcKey;
+        final JSON knowledgeFile = new JSONData(new S3FileLoader(event).load());
+        final JSONSchema jsonSchema = new JSONSchemaData(new FileLoader("/knowledgeSchema.json").load());
+
+        final Validator<JSONSchema, JSON> validator = new JSONValidator();
+        final Validated validate = validator.validate(jsonSchema, knowledgeFile);
+
+        logger.log("RESULT: " + validate);
+        return validate.toString();
     }
 }
--- lib/src/main/java/function/S3FileLoader.java
+++ lib/src/main/java/function/S3FileLoader.java
@@ -1,11 +1,24 @@
 package function;
 
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import kb_upload.Loader;
 
 public class S3FileLoader implements Loader<String> {
 
+    final S3EventNotification.S3Entity s3;
+
+    public S3FileLoader(final S3Event event) {
+        s3 = event.getRecords().get(0).getS3();
+    }
+
     @Override
     public String load() {
-        return null;
+        return AmazonS3ClientBuilder
+                .standard()
+                .build()
+                .getObjectAsString(s3.getBucket().getName(),
+                                   s3.getObject().getKey());
     }
 }
