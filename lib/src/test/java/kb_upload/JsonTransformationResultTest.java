5b10efcc81d36f0c335866550db6f8b18a74bc35
OODesigns <michael.lindre@oodesigns.co.uk>
2023-10-17 13:57:15 +0100

    Update configuration

--- lib/src/test/java/kb_upload/JsonTransformationResultTest.java
+++ lib/src/test/java/kb_upload/JsonTransformationResultTest.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class JsonTransformationResultTest {
+class
+JsonTransformationResultTest {
 
     @Test
     public void testMapWithEmptyList() {
