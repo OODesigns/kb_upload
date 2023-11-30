01341f208b2ecc1a50b7d3ebf53d274e71537a50
Michael Lindre <michael.lindre@oodesigns.co.uk>
2023-11-30 03:18:07 -0800

    Removed Made HandleValidation create only once, not each request

--- lib/src/main/java/aws/HandleValidation.java
+++ lib/src/main/java/aws/HandleValidation.java
@@ -12,25 +12,22 @@ import java.util.Map;
 import java.util.Optional;
 
 public class HandleValidation implements RequestHandler<Map<String, String>, ValidationResult> {
-    private final Validator<JSONSchema, JSON, ValidationResult> validator;
-    private final S3ObjectToJSON s3JSONFileDataTransformer;
     private static final Validator<JSONSchema, JSON, ValidationResult> defaultValidator
             = new JSONValidator(()->JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V4));
-    private static final S3Client s3Client = S3Client.builder().build();
-
-    private static final Retrievable<S3Object, Optional<InputStream>> defaultFileLoader = new S3StreamLoader(s3Client);
+    private static final Retrievable<S3Object, Optional<InputStream>> defaultFileLoader
+            = new S3StreamLoader(S3Client.builder().build());
+    private final ValidationHandler validationHandler;
 
     HandleValidation(final Validator<JSONSchema, JSON, ValidationResult> validator,
                      final Retrievable<S3Object, Optional<InputStream>> fileLoader){
 
-        this.validator = validator;
-        this.s3JSONFileDataTransformer = new S3JSONFileDataTransformer(fileLoader);
+        this.validationHandler = new ValidationHandler(validator, new S3JSONFileDataTransformer(fileLoader));
     }
 
     public HandleValidation() {this(defaultValidator, defaultFileLoader);}
 
      @Override
     public ValidationResult handleRequest(final Map<String, String> input, final Context context) {
-        return new HandleValidationContextDecorator(input, context, validator, s3JSONFileDataTransformer).handleRequest();
+        return validationHandler.handleRequest(input, context);
     }
 }
